b66dbd7787c571cb06784b72f087447c298a94b7
achibhossengit <mail.achibhossen@gmail.com>
2025-09-13 13:18:53 +0600

    setup basic project

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../pages/Shared/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../pages/Shared/Footer";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="text-center text-xl">this is home page</div>;
+};
+
+export default Home;
