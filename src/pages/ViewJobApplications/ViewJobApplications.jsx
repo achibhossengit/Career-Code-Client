67c37984422708fc2f9edfa5d65b4f0dd84ad6c6
achibhossengit <mail.achibhossen@gmail.com>
2025-09-15 11:19:00 +0600

    implement update status of a application feature

--- src/pages/ViewJobApplications/ViewJobApplications.jsx
+++ src/pages/ViewJobApplications/ViewJobApplications.jsx
@@ -1,8 +1,31 @@
+import axios from "axios";
 import { useLoaderData, useParams } from "react-router";
+import Swal from "sweetalert2";
 
 const ViewJobApplications = () => {
   const { job_id } = useParams();
   const applications = useLoaderData();
+  console.log(applications);
+  const handleStatusChange = (e, application_id) => {
+    console.log(e.target.value, application_id);
+
+    axios
+      .patch(`http://localhost:3000/applications/${application_id}`, {
+        status: e.target.value,
+      })
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          Swal.fire({
+            position: "top",
+            icon: "success",
+            title: "Stuatus updated successfully!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div>
       {applications.length} applications recieved for job id: {job_id}
@@ -12,11 +35,10 @@ const ViewJobApplications = () => {
             <th>#</th>
             <th>Applicant</th>
             <th>Job Title</th>
-            <th>Company</th>
             <th>Location</th>
             <th>LinkedIn</th>
             <th>GitHub</th>
-            <th>Resume</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
@@ -25,7 +47,6 @@ const ViewJobApplications = () => {
               <td>{index + 1}</td>
               <td>{app.applicant}</td>
               <td>{app.title}</td>
-              <td>{app.company}</td>
               <td>{app.location}</td>
               <td>
                 <a
@@ -48,14 +69,16 @@ const ViewJobApplications = () => {
                 </a>
               </td>
               <td>
-                <a
-                  href={app.resume}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 underline"
+                <select
+                  onChange={(e) => handleStatusChange(e, app._id)}
+                  defaultValue={app.status}
+                  className="select"
                 >
-                  Resume
-                </a>
+                  <option disabled={true}>Select Staus</option>
+                  <option>Pending</option>
+                  <option>Accepted</option>
+                  <option>Rejected</option>
+                </select>
               </td>
             </tr>
           ))}
