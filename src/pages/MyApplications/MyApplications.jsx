337361a19ee5d8aa7990f036e53e75eb44a97d08
achibhossengit <mail.achibhossen@gmail.com>
2025-09-16 07:35:47 +0600

    send firebase accessToken as request header

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -5,6 +5,9 @@ import ApplicationsTable from "./ApplicationsTable";
 
 const MyApplications = () => {
   const { user } = useAuth();
+
+  console.log(user);
+
   return (
     <div>
       <h2 className="text-xl font-bold text-center">
@@ -12,7 +15,7 @@ const MyApplications = () => {
       </h2>
       <Suspense fallback={'loading your applications'}>
         <ApplicationsTable
-          myApplicationsPromise={myApplicationsPromise(user.email)}
+          myApplicationsPromise={myApplicationsPromise(user.email, user.accessToken)}
         ></ApplicationsTable>
       </Suspense>
     </div>
