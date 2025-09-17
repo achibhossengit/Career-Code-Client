63af18d6c778e99938838c41c455ab74213f4cea
achibhossengit <mail.achibhossen@gmail.com>
2025-09-17 21:15:06 +0600

    handle error with axios response intercepter

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,8 @@ import useAuth from "./useAuth";
 const axiosInstance = axios.create({ baseURL: "http://localhost:3000/" });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const { user, signOutUser, setAuthLoading } = useAuth();
+
   axiosInstance.interceptors.request.use(
     (config) => {
       config.headers.authorization = `Bearer ${user.accessToken}`;
@@ -14,6 +15,27 @@ const useAxiosSecure = () => {
       console.log(error);
     }
   );
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error);
+      if (error.status === 401) {
+        console.log("Failed for Unauthorized Access 401");
+        signOutUser()
+          .then(() => console.log("signed out successfully"))
+          .catch((error) => console.log(error))
+          .finally(() => setAuthLoading(false));
+      }
+      if (error.status === 403)
+        console.log("Do something for forbiden error 403");
+
+      return Promise.reject(error);
+    }
+  );
+
   return axiosInstance;
 };
 
